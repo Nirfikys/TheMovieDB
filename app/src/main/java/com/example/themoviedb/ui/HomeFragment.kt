98fcd06c5cae6baa66914e0d57323a04af5401a8
Nirfik <54435539+nirfik@gmail.com>
2020-07-21 21:16:35 +0300

    theme

--- app/src/main/java/com/example/themoviedb/ui/HomeFragment.kt
+++ app/src/main/java/com/example/themoviedb/ui/HomeFragment.kt
@@ -1,6 +1,34 @@
 package com.example.themoviedb.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.themoviedb.databinding.HomeLayoutBinding
 
 class HomeFragment: Fragment() {
+    lateinit var binding: HomeLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = HomeLayoutBinding.inflate(inflater, container, false)
+        setupToolbar()
+        setupView()
+        return binding.root
+    }
+
+    private fun setupView(){
+
+    }
+
+    private fun setupToolbar(){
+        (activity as MainActivity).apply {
+            setSupportActionBar(binding.toolbar)
+            setupToolbar()
+        }
+    }
 }
--- app/src/main/java/com/example/themoviedb/ui/MainActivity.kt
+++ app/src/main/java/com/example/themoviedb/ui/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.themoviedb.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.themoviedb.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment), drawerLayout)
+    }
+
+    fun setupToolbar() {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
     }
 }
